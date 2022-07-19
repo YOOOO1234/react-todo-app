d95757a4aefba7a260be6f36e26ddab47143cba8
YOOOO1234 <tasakayo0930@gmail.com>
2022-07-19 20:54:14 +0900

    added box

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,14 +1,18 @@
-import { Button } from '@chakra-ui/react'
+import { Box, Button } from '@chakra-ui/react'
 export default function TodoItem({
   todo,
   onEditClick,
   onDeleteClick
 }) {
   return (
-    <li key={todo.id}>
-      {todo.text}
-      <Button ml={2} color='#4299E1' onClick={() => onEditClick(todo)}>編集</Button>
-      <Button ml={2} color='#48BB78' onClick={() => onDeleteClick(todo.id)}>完了</Button>
-    </li>
-  );
+    <Box bg="yellow.100">
+      <li key={todo.id}>
+        {todo.text}
+        <Button ml={2} color='#4299E1' onClick={() => onEditClick(todo)}>編集</Button>
+        <Button ml={2} color='#48BB78' onClick={() => onDeleteClick(todo.id)}>完了</Button>
+      </li>
+    </Box>
+  );      
+   
+
 }
